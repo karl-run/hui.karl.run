b7aaa7e59d59bce12f40f1aa3ea62f1f058c7a1c
Karl J. Overå <k@rl.run>
2019-02-22 20:43:40 +0100

    Initial commit

---
+++ src/hackerui/HackerUI.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const HackerUI = () => <div>TODO</div>
+
+export default HackerUI;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HackerUI from './hackerui/HackerUI';
+
+import './index.css';
+
+ReactDOM.render(<HackerUI />, document.getElementById('root'));
